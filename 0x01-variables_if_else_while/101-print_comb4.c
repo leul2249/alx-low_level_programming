260696f64db16b5b07b6d533d4611a17f4e66461
leul <leul2249@gmail.com>
2022-03-11 14:53:40 -0800

    variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,26 @@
  */
 int main(void)
 {
-	int n, m, 1;
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l = 50; l < 58; l++)
+			if (l > m && m > n)
 			{
-				if (l > m && m > n)
+				putchar(n);
+				putchar(m);
+				putchar(l);
+				if (n != 55 || m != 56)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(1);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
