6f403fb127f32ba349a40012b2340eb701e60d56
leul <leul2249@gmail.com>
2022-03-11 14:38:28 -0800

    hello,world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "programming is like building a multilingual puzzle
- *
- * Return: 0 if exited properly, non-zero otherwise
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
