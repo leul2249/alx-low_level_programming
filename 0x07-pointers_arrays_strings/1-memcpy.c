3453adff2287bb0894b5a550b60446321ece0109
leul2249 <99506095+leul2249@users.noreply.github.com>
2022-03-28 14:18:41 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _memcpy - copies memory area
- * @dest: destination of copy
- * @src: memory area to copy
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
@@ -12,9 +12,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++) 
+	for (i = 0; i < n; i++)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 	}
+
 	return (dest);
 }
